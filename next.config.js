40f4f60b1f1d1da35def49e7398f58bcaf5cd39f
K.Kiyokawa <koichi20110068@gmail.com>
2019-12-14 00:15:11 +0900

    refactor: omit args of webpack function in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 // const webpack = require('webpack')
 
 module.exports = {
-  webpack(config, _) {
+  webpack(config) {
     console.log(path.resolve(__dirname, './src'))
     config.resolve.alias['~'] = path.resolve(__dirname, './src')
     return config
